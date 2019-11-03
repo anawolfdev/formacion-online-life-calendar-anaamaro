2c4f2486c37ede766e26820a99bc4d15a60111cd
Ana Amaro <amarovazquezana@gmail.com>
2019-11-03 18:07:28 +0100

    npm run build docs

---
+++ docs/precache-manifest.9d242886249faa7e0997db3028f756e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22732a27b98c3e72cfba0522953f647a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "90871c7377422a70220c",
+    "url": "./static/css/main.065b6942.chunk.css"
+  },
+  {
+    "revision": "0b21c1bed14c2a75a7ae",
+    "url": "./static/js/2.9c27672a.chunk.js"
+  },
+  {
+    "revision": "90871c7377422a70220c",
+    "url": "./static/js/main.2376438d.chunk.js"
+  },
+  {
+    "revision": "1fa52392775535ad22f0",
+    "url": "./static/js/runtime-main.f3925cc8.js"
+  }
+]);
